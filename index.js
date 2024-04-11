ba7b7c1e7077b06d9bd3c3a1614322d48749c7ee
Surender <140454495+surender18@users.noreply.github.com>
2024-04-12 00:34:16 +0530

    Add files via upload
    added

---
+++ index.js
@@ -0,0 +1,102 @@
+import express from "express";
+import path from "path";
+import  mongoose  from "mongoose";
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+mongoose.connect("mongodb://localhost:27017",{
+    dbName:"Backend",
+})
+.then(()=> console.log("Database Connected"))
+.catch((e)=> console.log(e));
+
+const userSchema=new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String,
+});
+const User= mongoose.model("User",userSchema)
+
+const app = express();
+
+app.use(express.static(path.join(path.resolve(),"public")));
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+app.set("view engine ","ejs");
+
+const isAuthenticated=async(req,res,next)=>{
+    const {token} =req.cookies;
+    if(token){
+
+      const decoded  =jwt.verify(token,"sgnas")
+        req.user =await User.findById(decoded._id)
+        next();
+       }
+       else{
+        res.redirect("/login");
+       }
+}
+
+app.get("/",isAuthenticated,(req,res)=>{
+    console.log(req.user);
+    res.render("logout.ejs",{name:req.user.name});
+});
+
+app.get("/login",(req,res)=>{
+    res.render("login.ejs")
+})
+app.get("/register",(req,res)=>{
+    // console.log(req.user);
+    res.render("register.ejs");
+});
+app.post("/login",async(req,res)=>{
+    const {email,password}=req.body;
+    let user = await User.findOne({email});
+    if(!user) return res.redirect("/register");
+
+    const ismatch = await bcrypt.compare(password, user.password);
+    if(!ismatch) return res.render("login",{email,message:"Incorrect Password"});
+
+    const token=jwt.sign({ _id:user._id},"sgnas");
+    res.cookie("token",token,{
+        httpOnly : true,expires:new Date(Date.now()+60*1000)
+});
+res.redirect("/");
+})
+
+app.post("/register",async(req,res)=>{
+    const {name,email,password}=req.body;
+
+    let user = await User.findOne({email})
+    if(user){
+        return res.redirect("/login");
+
+    }
+    const hashedpass = await bcrypt.hash(password,10);
+   user = await User.create({
+        name,
+        email,
+        password:hashedpass,
+    });
+
+
+    const token=jwt.sign({ _id:user._id},"sgnas");
+    res.cookie("token",token,{
+        httpOnly : true,expires:new Date(Date.now()+60*1000)
+});
+res.redirect("/");
+});
+
+app.get("/logout",(req,res)=>{
+    res.cookie("token",null,{
+        httpOnly:true,
+    expires:new Date(Date.now()),
+    
+});
+res.redirect("/");
+});
+
+app.listen(5000,()=>{
+    console.log("server is working");
+});  
